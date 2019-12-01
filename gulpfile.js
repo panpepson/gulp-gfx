ced776093647282a1323e9ee2d77622fbd324971
Piotr Trochymiak <Piotr@Trochymiak.net>
2019-12-01 15:36:46 +0100

    clering in sourece file .js and new resetting sources folders

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,15 @@ var imageminMozjpeg = require('imagemin-mozjpeg');
 //var imageminGuetzli = require('imagemin-guetzli');
 //var sequence = require('run-sequence');
 var sequence = require('gulp-sequence')
+
 // ======================== obrazki =======================
 
 var obrazki = {
-     imgin: '/home/olo/Pulpit/LWL/00/*.{jpg,jpeg,png,gif,JPG}',
-     imgout: '/home/olo/Pulpit/LWL/00/1',
-     imgin2: '/home/olo/Pulpit/LWL/00/0/*.{jpg,jpeg,png,gif}',
-     mjpgIn: '/home/olo/Pulpit/LWL/00/1/*.{jpg,jpeg,png,gif,JPG}',
-     mjpgOut: '/home/olo/Pulpit/LWL/00/0/',
+     imgin: '/home/00/*.{jpg,jpeg,png,gif,JPG}',
+     imgout: '/home//00/1',
+     imgin2: '/home/00/0/*.{jpg,jpeg,png,gif}',
+     mjpgIn: '/home/00/1/*.{jpg,jpeg,png,gif,JPG}',
+     mjpgOut: '/home/00/0/',
      };
 
 gulp.task('resize', function (done) {
@@ -40,34 +41,3 @@ gulp.task('mjpg', function (done ) {
 });
 
 gulp.task('gfx', gulp.series(['resize'],'mjpg'), function() {
-
-//    gulp.task('gfx',['resize'], function() {
-    // nic nie dzila :(
-          //gulp.series(['resize']);
-//	  gulp.watch('/home/olo/Pulpit/LWL/00/*.{jpg,jpeg,png,gif,JPG}','resize');
-//	  gulp.watch('/home/olo/Pulpit/LWL/00/0/*.{jpg,jpeg,png,gif,JPG}','mjpg');
-
-//	 gulp.watch('resize').gulp.start('mjpg');
-//	  done();
-        });
-
-/*
-gulp.task('gfx', sequence(['resize'],'mjpg'))
-gulp.task('mozjpeg', () =>
-    gulp.src('src/*.jpg')
-    .pipe(imagemin([imageminMozjpeg({
-        quality: 85
-    })]))
-    .pipe(gulp.dest('dist'))
-);
-
-gulp.task('img', function() {
-  return gulp.src(obrazki.imgin)
-    .pipe(changed(obrazki.imgin))
-    .pipe(imagemin())
-    .pipe(gulp.dest(obrazki.imgin));
-});
-*/
-
-
-
